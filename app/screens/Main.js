734fb21218dda0db0202669db187667c6865b780
SeanNolting <113694273+SeanNolting@users.noreply.github.com>
2024-04-29 12:54:06 -0500

    Good progress on displaying the user data
    I was able to display one user based on their docID so now I just need to adjust it from being hard coded. Read google doc.

--- app/screens/Main.js
+++ app/screens/Main.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet } from 'react-native';
 import { IconButton } from 'react-native-paper';
 import colors from '../config/colors';
 import firebase from 'firebase/app';
-import 'firebase/firestore';
+import {doc, getDoc} from 'firebase/firestore';
 import 'firebase/database'
 import { FIREBASEAPP, db } from '../../Firebase/config';
 import config from '../../Firebase/config'
@@ -11,82 +11,25 @@ import MyButton from '../components/MyButton';
 
    
 export default function Main() {
-  // const DisplayData = () => {
-  //   const [data, setData] = useState(null);
-  //   // const [currentUserDocId, setCurrentUserDocId] = useState(null);
 
-  //   useEffect(() => {
-  //     const fetchData = async () => {
-  //       try {
-  //         firebase.auth().onAuthStateChanged((user) => {
-  //           if (user) {
-  //             const userId = user.uid;
-  //             const userDocRef = firebase.firestore().collection('userInfo').doc(userId);
-  //             userDocRef.get().then((userDocSnapshot) => {
-  //               if (userDocSnapshot.exists) {
-  //                 const userData = userDocSnapshot.data();
-  //                 setData(userData);
-  //               } else {
-  //                 console.log('No user found with the given ID!');
-  //               }
-  //             }).catch((error) => {
-  //               console.error('Error fetching user document: ', error);
-  //             });
-  //           } else {
-  //             console.log('No user signed in.');
-  //           }
-  //         });
-  //       } catch (error) {
-  //         console.error('Error fetching document: ', error);
-  //       }
-  //     };
-  //     fetchData();
-    // }, []);
+    const [userData, setUserData] = useState(null);
 
-
-      // const [userData, setUserData] = useState([]);
-      // const [currentUserIndex, setCurrentUserIndex] = useState(0);
-
-      // useEffect(() => {
-      //   const grabUserData = () => {
-      //     firebase.database().ref('userInfo').once('value', (snapshot) => {
-      //       const data = snapshot.val();
-      //       if (data) {
-      //         const userDataArray = Object.values(data);
-      //         setUserData(userDataArray);
-      //       }
-      //     });
-      //   };
-      //   grabUserData();
-      // }, []);
-    
-      // const showNextUser = () => {
-      //   setCurrentUserIndex((currentUserIndex + 1) % userData.length);
-      // };
-    
-
-      // const [otherUsers, setOtherUsers] = useState([]);
-      // useEffect(() => {
-      //   const fetchOtherUsers = async () => {
-      //     try{
-      //       const userRef = firebase.firestore().collection('userInfo');
-      //       const snapshot = await userRef.get();
-      //       const userList = [];
-      //       snapshot.forEach(doc => {
-      //         if(doc.id !== firebase.auth().currentUser.uid){
-      //           userList.push({ id: doc.id, ...doc.data()});
-      //         }
-      //       });
-      //       setOtherUsers(userList);
-      //     } catch(error){
-      //       console.error("Error fetching other users", error);
-      //     }
-      //   };
-
-      //   fetchOtherUsers();
-      // }, []);
-    
-
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const docRef = doc(db, "userInfo", "Bske2zvRYPMICKUhBNNA");
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          setUserData(docSnap.data());
+        } else {
+          console.log("No such document!");
+        }
+      } catch (error) {
+        console.error("Error fetching document: ", error);
+      }
+    };
+    fetchUserData();
+  }, []);
 
     return (
       <View style={styles.container}>
@@ -97,26 +40,19 @@ export default function Main() {
             color="black"
             size={45}
           />
-          {/* {otherUsers.map(user => (
-            <View
-            key={user.id}
-            >
-              
-              <View/>
-          ))} */}
-          {/* <UserDataDisplay user={userData[currentUserIndex]}/> */}
           <IconButton
             style={styles.rightArrow}
             icon="arrow-right"
             color="black"
             size={45}
-            // onPress={showNextUser}
+      
           />
           
         </View> 
-          <Text>Name: </Text>
-          <Text>Major:</Text>
-          <Text>Biography: </Text>
+          <Text>Name: {userData ? `${userData.firstName} ${userData.lastName}` : ""} </Text>
+          <Text>Major:  {userData ? userData.major : ""}</Text>
+          <Text>Biography:  {userData ? userData.bio : ""} </Text>
+          <Text>Hobbies:  {userData ? userData.hobbies + "" : ""} </Text>
           <MyButton color={colors.delftBlue}
             title={"View more"}
           />
