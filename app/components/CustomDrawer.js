61925ba419de52fdc494daf6359bcea6014491e4
SeanNolting <113694273+SeanNolting@users.noreply.github.com>
2024-05-01 12:54:00 -0500

    Tested displaying data in the drawerview

--- app/components/CustomDrawer.js
+++ app/components/CustomDrawer.js
@@ -1,16 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import {View, Text, Image} from 'react-native'
 import {DrawerContentScrollView, DrawerItemList} from "@react-navigation/drawer"
+import {collection, doc, getDoc, getDocs, query, where} from 'firebase/firestore';
+import 'firebase/database'
+import { FIREBASEAPP, db } from '../../Firebase/config';
+import config from '../../Firebase/config'
 
 const CustomDrawer = (props) => {
+    const [userData, setUserData] = useState(null);
+
+    useEffect(() => {
+      const fetchUserData = async () => {
+        try {
+          const docRef = doc(db, "userInfo", "Bske2zvRYPMICKUhBNNA");
+          const docSnap = await getDoc(docRef);
+          if (docSnap.exists()) {
+            setUserData(docSnap.data());
+          } else {
+            console.log("No such document!");
+          }
+        } catch (error) {
+          console.error("Error fetching document: ", error);
+        }
+      };
+      fetchUserData();
+    }, []);
     return(
         <View style={{flex:1}} >
         <DrawerContentScrollView {...props} contentContainerStyle={{backgroundColor:"#FFFFFF"}}>
             <Image source={require("../../images/Gatorade.png")}
             style={{height: 100, width: 100, borderRadius:50, marginBottom: 10, marginLeft: 5, borderColor:"#000000", borderWidth: 3}}
             />
-            <Text style={{color:"#000000", marginBottom: 10,  marginLeft: 10, fontSize: 16}}>Name: placeholder</Text>
-            <Text style={{color:"#000000", marginBottom: 10, marginLeft: 10, fontSize: 16}}>Major: placeholder</Text>
+            <Text style={{color:"#000000", marginBottom: 10,  marginLeft: 10, fontSize: 16}}>Name: {userData ? `${userData.firstName} ${userData.lastName}` : ""}</Text>
+            <Text style={{color:"#000000", marginBottom: 10, marginLeft: 10, fontSize: 16}}>Major:  {userData ? userData.major : ""}</Text>
             <DrawerItemList {...props} />
         </DrawerContentScrollView>
         <View>
